db71c20c74fa7c50c1cc7a8125d591c70daff9bd
sonusharmagaur-blip <sonu.sharma.gaur@gmail.com>
2026-08-31 18:47:59 +0530

    Refresh app for improved share card v27

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker: caches the app shell so RailwayLogbook works fully offline.
 // Bump CACHE_NAME whenever any precached file changes so clients pick up the update.
-const CACHE_NAME = "railwaylogbook-v26";
+const CACHE_NAME = "railwaylogbook-v27";
 
 const PRECACHE_URLS = [
   "./",
@@ -22,7 +22,7 @@ const PRECACHE_URLS = [
   "./js/exportCard.js",
   "./js/rangeReport.js",
   "./js/drive.js",
-  "./wap7-share-watermark.png",
+  "./wap7-share-background-v2.png",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
   "./icons/apple-touch-icon.png",
