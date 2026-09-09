6f4253dc721939e2a6b789467a2ea69ba2e17530
sonusharmagaur-blip <sonu.sharma.gaur@gmail.com>
2026-09-09 11:57:18 +0530

    Publish updated WhatsApp 4:5 share cards

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker: caches the app shell so RailwayLogbook works fully offline.
 // Bump CACHE_NAME whenever any precached file changes so clients pick up the update.
-const CACHE_NAME = "railwaylogbook-v51";
+const CACHE_NAME = "railwaylogbook-v52";
 
 const PRECACHE_URLS = [
   "./",
@@ -20,6 +20,7 @@ const PRECACHE_URLS = [
   "./js/settings.js",
   "./js/sheets.js",
   "./js/exportCard.js",
+  "./js/sharePhoto.js",
   "./js/rangeReport.js",
   "./js/drive.js",
   "./wap7-share-background.png",
